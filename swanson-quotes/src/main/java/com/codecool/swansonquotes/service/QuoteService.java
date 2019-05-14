8de54f7c858013ba245c3349a51d7f1d9a93de83
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-05-14 16:45:47 +0200

    created QuoteService & QuoteController for it

--- swanson-quotes/src/main/java/com/codecool/swansonquotes/service/QuoteService.java
+++ swanson-quotes/src/main/java/com/codecool/swansonquotes/service/QuoteService.java
@@ -1,4 +1,12 @@
 package com.codecool.swansonquotes.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class QuoteService {
+
+    public String getRandomQuote() {
+        return "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+    }
+
 }
