6c25ad7b024655d0427fe88394179e8d72ede499
Thao Phuong Lam <lphuongt930906@gmail.com>
2019-05-15 16:07:00 +0200

    created QuoteResult class

--- swanson-quotes/src/main/java/com/codecool/swansonquotes/model/QuoteResult.java
+++ swanson-quotes/src/main/java/com/codecool/swansonquotes/model/QuoteResult.java
@@ -1,4 +1,14 @@
 package com.codecool.swansonquotes.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class QuoteResult {
+
+    private String quote;
+
 }
