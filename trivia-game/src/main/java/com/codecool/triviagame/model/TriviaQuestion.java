5fe167e6182ec87ee4bd3ed8ab36bafe2cc9d41b
istvan <istvan.z@freemail.hu>
2019-05-16 15:16:23 +0200

    Removed superfluous Json data

--- trivia-game/src/main/java/com/codecool/triviagame/model/TriviaQuestion.java
+++ trivia-game/src/main/java/com/codecool/triviagame/model/TriviaQuestion.java
@@ -3,10 +3,7 @@ package com.codecool.triviagame.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -14,9 +11,6 @@ import java.util.List;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-        "category",
-        "type",
-        "difficulty",
         "question",
         "correct_answer",
         "incorrect_answers"
@@ -24,15 +18,6 @@ import java.util.List;
 @Component
 @Data
 public class TriviaQuestion {
-
-    @JsonProperty("category")
-    public String category;
-
-    @JsonProperty("type")
-    public String type;
-
-    @JsonProperty("difficulty")
-    public String difficulty;
 
     @JsonProperty("question")
     public String question;
