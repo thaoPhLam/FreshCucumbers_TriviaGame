9eacc45f290da84c5430036f660396a3422f0503
istvan <istvan.z@freemail.hu>
2019-05-16 14:45:38 +0200

    Added annotations and dependency

--- trivia-game/src/main/java/com/codecool/triviagame/TriviaGameApplication.java
+++ trivia-game/src/main/java/com/codecool/triviagame/TriviaGameApplication.java
@@ -2,10 +2,12 @@ package com.codecool.triviagame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TriviaGameApplication {
 
     public static void main(String[] args) {
